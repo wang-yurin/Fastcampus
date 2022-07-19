2062252c0854967c0897aac753061f485a9ad46c
Wang-yurin <king_1994@naver.com>
2022-07-19 12:45:05 +0900

    키보드 인풋 기능

--- virtual keyboard/src/js/keyboard.js
+++ virtual keyboard/src/js/keyboard.js
@@ -2,6 +2,9 @@ export class Keyboard {
   #switchEl;
   #fontSelectEl;
   #containerEl;
+  #keyboardEl;
+  #inputGroupEl;
+  #inputEl;
   constructor() {
     this.#assignElement();
     this.#addEvent();
@@ -12,11 +15,17 @@ export class Keyboard {
     this.#containerEl = document.querySelector(".container");
     this.#switchEl = this.#containerEl.querySelector("#switch");
     this.#fontSelectEl = this.#containerEl.querySelector("#font");
+    this.#keyboardEl = this.#containerEl.querySelector(".keyboard");
+    this.#inputGroupEl = this.#containerEl.querySelector(".input-group");
+    this.#inputEl = this.#inputGroupEl.querySelector(".input");
   }
 
   #addEvent() {
     this.#switchEl.addEventListener("change", this.#onChangeTheme);
     this.#fontSelectEl.addEventListener("change", this.#onChangeFont);
+    document.addEventListener("keydown", this.#onKeyDown.bind(this));
+    document.addEventListener("keyup", this.#onKeyUp.bind(this));
+    this.#inputEl.addEventListener("input", this.#onInput);
   }
 
   #onChangeTheme(event) {
@@ -27,5 +36,26 @@ export class Keyboard {
   }
   #onChangeFont(event) {
     document.body.style.fontFamily = event.target.value;
+  }
+  #onInput(event) {
+    //   console.log(this.#inputEl.value);
+    event.target.value = event.target.value.replace(/[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/, "");
+  }
+  #onKeyDown(event) {
+    // console.log(event.code);
+    // console.log(/[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/.test(event.key)); //한글이면 true, 영문이면 false
+    this.#inputGroupEl.classList.toggle(
+      "error",
+      /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/.test(event.key)
+    );
+
+    this.#keyboardEl
+      .querySelector(`[data-code=${event.code}]`)
+      ?.classList.add("active");
+  }
+  #onKeyUp(event) {
+    this.#keyboardEl
+      .querySelector(`[data-code=${event.code}]`)
+      ?.classList.remove("active");
   }
 }
