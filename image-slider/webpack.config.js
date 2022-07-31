9a718e6cc2b195b4526ce6d3e99fc4e910d4cc6d
wang-yurin <king_1994@naver.com>
2022-07-31 15:38:01 +0900

    초기 설정

--- image-slider/webpack.config.js
+++ image-slider/webpack.config.js
@@ -1,44 +1,46 @@
-const path = require("path");
-const TerserPlugin = require("terser-webpack-plugin");
-const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 module.exports = {
-  entry: "./src/js/index.js",
+  entry: './src/js/index.js',
   output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "./dist"),
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, './dist'),
     clean: true,
   },
   devtool: 'source-map',
-  mode: "development",
+  mode: 'development',
   devServer: {
-    host: "localhost",
+    host: 'localhost',
     port: 8080,
     open: true,
     watchFiles: 'index.html',
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: "project",
-      template: "./index.html",
-      inject: "body",
-      favicon: "./favicon.ico"
+      title: 'project',
+      template: './index.html',
+      inject: 'body',
+      favicon: './favicon.ico',
     }),
-    new MiniCssExtractPlugin({ filename: "style.css" }),
+    new MiniCssExtractPlugin({ filename: 'style.css' }),
   ],
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader"],
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.jpeg$/,
+        type: 'asset/inline',
       },
     ],
   },
   optimization: {
-    minimizer: [
-      new TerserPlugin(),
-      new CssMinimizerPlugin()
-    ]
+    minimizer: [new TerserPlugin(), new CssMinimizerPlugin()],
   },
 };
