8c0b7770f4cf9676589c788fd6db40c034434294
wang-yurin <king_1994@naver.com>
2022-08-01 20:03:12 +0900

    next, prev 버튼 구현

---
+++ image-slider/src/js/imageSlider.js
@@ -0,0 +1,67 @@
+export default class ImageSlider {
+  #currentPosition = 0;
+
+  #slideNumber = 0;
+
+  #slideWidth = 0;
+
+  sliderWrapEl;
+
+  sliderListEl;
+
+  nextBtnEl;
+
+  previousBtnEl;
+
+  constructor() {
+    this.asssignElement();
+    this.initSliderNumber();
+    this.initSliderWidth();
+    this.initSliderListWidth();
+    this.addEvent();
+  }
+
+  asssignElement() {
+    this.sliderWrapEl = document.getElementById('slider-wrap');
+    this.sliderListEl = this.sliderWrapEl.querySelector('#slider');
+    this.nextBtnEl = this.sliderWrapEl.querySelector('#next');
+    this.previousBtnEl = this.sliderWrapEl.querySelector('#previous');
+  }
+
+  initSliderNumber() {
+    this.#slideNumber = this.sliderListEl.querySelectorAll('li').length;
+  }
+
+  initSliderWidth() {
+    this.#slideWidth = this.sliderListEl.clientWidth;
+  }
+
+  initSliderListWidth() {
+    this.sliderListEl.style.width = `${this.#slideNumber * this.#slideWidth}px`;
+  }
+
+  addEvent() {
+    this.nextBtnEl.addEventListener('click', this.moveToRight.bind(this));
+    this.previousBtnEl.addEventListener('click', this.moveToLeft.bind(this));
+  }
+
+  moveToRight() {
+    this.#currentPosition++;
+    if (this.#currentPosition === this.#slideNumber) {
+      this.#currentPosition = 0;
+    }
+    this.sliderListEl.style.left = `-${
+      this.#slideWidth * this.#currentPosition
+    }px`;
+  }
+
+  moveToLeft() {
+    this.#currentPosition--;
+    if (this.#currentPosition === -1) {
+      this.#currentPosition = this.#slideNumber - 1;
+    }
+    this.sliderListEl.style.left = `-${
+      this.#slideWidth * this.#currentPosition
+    }px`;
+  }
+}
--- image-slider/src/js/index.js
+++ image-slider/src/js/index.js
@@ -1,1 +1,4 @@
 import '../css/style.css';
+import ImageSlider from './imageSlider.js';
+
+new ImageSlider();
