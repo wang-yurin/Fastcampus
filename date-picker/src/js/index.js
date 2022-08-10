2917b8969c241cbfdc8229d65cd6ef8580b822d7
wang-yurin <king_1994@naver.com>
2022-08-10 17:28:18 +0900

    날짜, 토요일 일요일 색상 동적으로 구현

--- date-picker/src/js/index.js
+++ date-picker/src/js/index.js
@@ -1,1 +1,139 @@
-console.log("hi");
+class DatePicker {
+  monthData = [
+    "January",
+    "Fabruary",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "Nobember",
+    "December",
+  ];
+
+  #calendarDate = {
+    data: "",
+    date: 0,
+    month: 0,
+    year: 0,
+  };
+
+  selectedDate = {
+    data: "",
+    date: 0,
+    month: 0,
+    year: 0,
+  };
+
+  datePickerEl;
+  dateInputEl;
+  calendarEl;
+  calendarMonthEl;
+  nextBtnEl;
+  prevBtnEl;
+  calendarDatesEl;
+
+  constructor() {
+    this.initCalendarDate();
+    this.assignElement();
+    this.addEvent();
+  }
+
+  initCalendarDate() {
+    const data = new Date();
+    const date = data.getDate();
+    const month = data.getMonth();
+    const year = data.getFullYear();
+    this.#calendarDate = {
+      data,
+      date,
+      month,
+      year,
+    };
+  }
+
+  assignElement() {
+    this.datePickerEl = document.getElementById("date-picker");
+    this.dateInputEl = this.datePickerEl.querySelector("#date-input");
+    this.calendarEl = this.datePickerEl.querySelector("#calendar");
+    this.calendarMonthEl = this.calendarEl.querySelector("#month");
+    this.monthContentEl = this.calendarMonthEl.querySelector("#content");
+    this.nextBtnEl = this.calendarMonthEl.querySelector("#next");
+    this.prevBtnEl = this.calendarMonthEl.querySelector("#prev");
+    this.calendarDatesEl = this.calendarEl.querySelector("#dates");
+  }
+
+  addEvent() {
+    this.dateInputEl.addEventListener("click", this.toggleCalendar.bind(this));
+  }
+
+  toggleCalendar() {
+    this.calendarEl.classList.toggle("active");
+    this.updateMonth();
+    this.updateDates();
+  }
+
+  updateMonth() {
+    this.monthContentEl.textContent = `${this.#calendarDate.year} ${
+      this.monthData[this.#calendarDate.month]
+    }`;
+  }
+
+  updateDates() {
+    this.calendarDatesEl.innerHTML = "";
+    const numberOfDates = new Date(
+      this.#calendarDate.year,
+      this.#calendarDate.month + 1,
+      0
+    ).getDate();
+
+    const fragment = new DocumentFragment();
+    for (let i = 0; i < numberOfDates; i++) {
+      const dateEl = document.createElement("div");
+      dateEl.classList.add("date");
+      dateEl.textContent = i + 1;
+      dateEl.dataset.date = i + 1;
+      fragment.appendChild(dateEl);
+    }
+    fragment.firstChild.style.gridColumnStart =
+      new Date(this.#calendarDate.year, this.#calendarDate.month, 1).getDay() +
+      1;
+    this.calendarDatesEl.appendChild(fragment);
+    this.colorSaturday();
+    this.colorSunday();
+  }
+
+  colorSaturday() {
+    const saturdayEls = this.calendarDatesEl.querySelectorAll(
+      `.date:nth-child(7n + ${
+        7 -
+        new Date(this.#calendarDate.year, this.#calendarDate.month, 1).getDay()
+      })`
+    );
+    for (let i = 0; i < saturdayEls.length; i++) {
+      saturdayEls[i].style.color = "blue";
+    }
+  }
+
+  colorSunday() {
+    const sundayEls = this.calendarDatesEl.querySelectorAll(
+      `.date:nth-child(7n + ${
+        (8 -
+          new Date(
+            this.#calendarDate.year,
+            this.#calendarDate.month,
+            1
+          ).getDay()) %
+        7
+      })`
+    );
+    for (let i = 0; i < sundayEls.length; i++) {
+      sundayEls[i].style.color = "red";
+    }
+  }
+}
+
+new DatePicker();
