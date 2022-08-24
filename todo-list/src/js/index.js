8c83504fe9d2c291ea7477fa78bc672eee84acaa
wang-yurin <king_1994@naver.com>
2022-08-24 16:33:22 +0900

    add button

--- todo-list/src/js/index.js
+++ todo-list/src/js/index.js
@@ -1,2 +1,31 @@
 import "../scss/style.scss";
 import "@fortawesome/fontawesome-free/js/all.min.js";
+
+class TodoList {
+  constructor() {
+    this.assignElement();
+    this.addEvent();
+  }
+  assignElement() {
+    this.inputContainerEl = document.getElementById("input-container");
+    this.inputAreaEl = this.inputContainerEl.querySelector("#input-area");
+    this.todoInputEl = this.inputAreaEl.querySelector("#todo-input");
+    this.addBtnEl = this.inputAreaEl.querySelector("#add-btn");
+  }
+  addEvent() {
+    this.addBtnEl.addEventListener("click", this.onClickAddBtn.bind(this));
+  }
+
+  onClickAddBtn() {
+    if (this.todoInputEl.value.length === 0) {
+      alert("내용을 입력해주세요.");
+      return;
+    }
+
+    this.createTodoElement(this.todoInputEl.value.length);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const todoList = new TodoList();
+});
